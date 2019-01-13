a61ed30f42931c4b22a510977bbadb5df3e8f49f
efe tiryaki <m.efetiryaki@gmail.com>
2019-01-13 21:27:10 +0100

    test functions are updated

---
+++ ode_solver_test/src/pendulumScope_test.cpp
@@ -0,0 +1,62 @@
+
+
+#include <iostream>
+#include <vector>
+#include <Eigen/Core>
+#include "ode_solver/RungeKutta.h"
+#include "ode_solver/Simulator.h"
+#include "plotgui/ScopeGui.h"
+#include <boost/bind.hpp>
+
+using namespace ODE;
+using namespace std;
+//using namespace std::placeholders;
+
+Eigen::VectorXd pendulumEquation(double time, Eigen::VectorXd x){
+  Eigen::MatrixXd A = Eigen::MatrixXd::Zero(2,2);
+  A << 0, 1, -9.81*1 , -1.0 ;
+  return A*x ;
+
+}
+
+int main() {
+  // Scope
+  int argc;
+  char **argv;
+  QApplication app(argc, argv);
+  ScopeGui gui;
+
+  // Solver
+  RungeKutta* solver = new RungeKutta();
+  solver->setDynamics(boost::bind(&pendulumEquation, _1, _2));
+
+  double maxTime = 10.0;
+  int stepNumber = 1000;
+  double stepSize = 0.01;
+  Eigen::Vector2d initValue;
+  initValue << 0.5, 0;
+  // Simulator
+  Simulator sim;
+  sim.initilize(solver);
+  sim.setRealTimeFactor(1);
+
+  sim.start(initValue, maxTime, stepNumber);
+  //sim.start(initValue, stepSize);
+  gui.setScopeNumber(2);
+  std::vector<std::string> legendNames(0);
+  legendNames.push_back("position");
+  gui.setXAxisRange(5);
+  gui.setYAxis(-0.5,0.5);
+  gui.addTimeSignal(boost::bind(&Simulator::getBuffer,&sim,-1));
+  gui.addStopFunction(boost::bind(&Simulator::setStop,&sim, _1));
+  gui.addClearFunction(boost::bind(&Simulator::clearBuffer,&sim));
+  gui.addGraph(0, boost::bind(&Simulator::getBuffer,&sim, 0));
+  gui.legend(0,legendNames);
+  gui.addGraph(1, boost::bind(&Simulator::getBuffer,&sim, 1));
+  legendNames[0] = "velocity" ;
+  gui.legend(1,legendNames);
+
+  gui.show();
+
+  return app.exec();
+}
---
+++ ode_solver_test/src/pendulum_test.cpp
@@ -0,0 +1,59 @@
+
+
+#include <iostream>
+#include <vector>
+#include <Eigen/Core>
+#include "ode_solver/OdeSolver.h"
+#include "ode_solver/RungeKutta.h"
+#include "plotgui/PlotGui.h"
+#include <boost/bind.hpp>
+
+using namespace ODE;
+using namespace std;
+//using namespace std::placeholders;
+
+Eigen::VectorXd pendulumEquation(double time, Eigen::VectorXd x){
+  Eigen::MatrixXd A = Eigen::MatrixXd(2,2);
+  A << 0, 1, -9.81*1 , -1.0 ;
+  return A*x ;
+
+}
+
+int main() {
+
+  RungeKutta solver = RungeKutta();
+  solver.setDynamics(boost::bind(&pendulumEquation, _1, _2));
+
+  double maxTime = 10.0;
+  int stepNumber = 1000;
+  Eigen::Vector2d initValue;
+  initValue << 0.6, -0.2;
+
+  solver.solve(initValue, maxTime, stepNumber);
+  std::vector<Eigen::VectorXd> solution = solver.getSolution();
+  Eigen::VectorXd t = solution.back();
+  //cout<< solver.getSolution() <<endl;
+
+  // Scope
+  int argc;
+  char **argv;
+  QApplication app(argc, argv);
+  PlotGui gui;
+
+  gui.subplot(2,1,0);
+  gui.plot(t,solution[0]);
+  gui.fillBelow(QColor(0,0,255,75));
+  gui.yLabel("Angle(rad)");
+  std::vector<std::string> lName(1);
+  lName[0] ="pendulum angle";
+  gui.legend(lName);
+  gui.subplot(2,1,1);
+  gui.plot(t,solution[1],Qt::red);
+  gui.fillBelow(QColor(255,0,0,75));
+  gui.xLabel("Time(s)");
+  gui.yLabel("Angular Velocity(rad/s)");
+  lName[0] ="pendulum angular velocity" ;
+  gui.legend(lName);
+  gui.show();
+  return app.exec();
+}
